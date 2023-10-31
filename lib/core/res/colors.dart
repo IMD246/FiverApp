574480a22967c88a2a6e867772c1e5672cbef49c
ThanhDuy <tduy24699@gmail.com>
2023-10-31 11:38:22 +0700

    add missing colors file

---
+++ lib/core/res/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color colorPrimary = const Color(0xffDB3022);
+Color color2AA952 = const Color(0xff2AA952);
+Color colorEF3651 = const Color(0xffEF3651);
+Color colorWhite = const Color(0xffFFFFFF);
+Color colorBlack = const Color(0xff1E1F28);
+Color color222222 = const Color(0xff222222);
