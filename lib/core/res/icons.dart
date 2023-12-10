47b6547499e1279d4f7fba03b7b6bfed89b820c8
ThanhDuy <tduy24699@gmail.com>
2023-12-11 00:14:38 +0700

    cicd test 11

---
+++ lib/core/res/icons.dart
@@ -0,0 +1,11 @@
+class DIcons {
+  static String get account => "assets/icons/ic_account.svg";
+  static String get addToFavorite => "assets/icons/ic_add_to_favorite.svg";
+  static String get arrowRight => "assets/icons/ic_arrow_right.svg";
+  static String get google => "assets/icons/ic_google.svg";
+  static String get heart => "assets/icons/ic_heart.svg";
+  static String get home => "assets/icons/ic_home.svg";
+  static String get profile => "assets/icons/ic_profile.svg";
+  static String get shop => "assets/icons/ic_shop.svg";
+  static String get shoppingBag => "assets/icons/ic_shopping_bag.svg";
+}
---
+++ lib/core/res/images.dart
@@ -0,0 +1,3 @@
+class DImages {
+  static String get bgCardCheckIn => "assets/images/img_bg_card_check_in.png";
+}
